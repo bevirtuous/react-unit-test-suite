19596a45df5dc631c18656920542fb5189141922
devbucket <florian.mueller81@gmx.de>
2018-01-12 16:55:30 +0100

    Fixed pre-processor to find babelrc throug deep hierarchy.

--- config/pre-processor.js
+++ config/pre-processor.js
@@ -1,10 +1,9 @@
-const { readFileSync } = require('fs');
-const { resolve } = require('path');
 const { createTransformer } = require('babel-jest');
+const findBabelConfig = require('find-babel-config');
 const { safeLoad } = require('js-yaml');
 const stripComments = require('strip-json-comments');
 
-const babelConfigString = readFileSync(resolve(process.cwd(), './.babelrc'), 'utf8');
-const babelConfig = safeLoad(stripComments(babelConfigString));
+const { config } = findBabelConfig.sync(process.cwd());
+const babelConfig = safeLoad(stripComments(config));
 
 module.exports = createTransformer(babelConfig);
